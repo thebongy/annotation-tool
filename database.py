064b1398124973c1e60565cb6250ae8ee9fed4f3
Rishit Bansal <rishit.bansal0@gmail.com>
2018-06-25 10:27:59 +0530

    feat

---
+++ database.py
@@ -0,0 +1,68 @@
+import uuid
+import sqlite3
+import os
+from exceptions import InvalidUsage
+from flask import jsonify
+
+class Database:
+    def __init__(self, db):
+        self.db = db
+        self.cursor = self.db.cursor()
+    def create_new_project(self, projectName):
+        projectID = str(uuid.uuid4())
+        print("Creating project", projectID)
+        self.dbExec("INSERT INTO PROJECTS VALUES (?,?)", (projectID, projectName))
+        return projectID
+
+    def dbExec(self, query, args=None):
+        print(args)
+        out = self.cursor.execute(query, args).fetchall()
+        self.db.commit()
+        return out
+
+    def get_project_by_ID(self, ID):
+        project = self.dbExec("SELECT * FROM PROJECTS WHERE ID=?", (ID,))[0]
+        if (project is None):
+            return None
+        return {
+            "id": project[0],
+            "name": project[1],
+            "files": self.get_project_files(ID)
+        }
+    def get_project_files(self, projectID):
+        filesData = self.dbExec("SELECT * FROM FILES WHERE PROJECT=?", (projectID,))
+        files = []
+        for file in filesData:
+            files.append({
+                "id": file[0],
+                "project": file[1],
+                "type": file[2],
+                "original": file[3]
+                })
+        return files
+    def add_file_to_project(self, file, projectID):
+        fileName = file.filename
+        ext = fileName[fileName.find("."):]
+        print(fileName, "with ext", ext)
+
+        if ext.lower() in (".jpg", ".png"):
+            file_type = "image"
+        elif ext.lower() in (".mp4", ".webm", ".avi", ".wmv", ".mov"):
+            file_type = "video"
+        else:
+            file_type = "unknown"
+            raise InvalidUsage("File specified is not a video/image")
+
+        fileID = str(uuid.uuid4())
+
+        new_file_name = fileID + ext
+        new_file_path = os.path.join(".", "data", new_file_name)
+        print("saving", fileName, "to", new_file_path)
+        file.save(new_file_path)
+
+        self.dbExec("INSERT INTO FILES VALUES (?,?,?,?)", (new_file_name, projectID, file_type, fileName))
+        return {"original": fileName, "id":new_file_name, "type": file_type}
+
+    def delete_file(self, fileID):
+        self.dbExec("DELETE FROM FILES WHERE ID=?", (fileID,))
+        os.remove(os.path.join("data", fileID))
--- server.py
+++ server.py
@@ -1,10 +1,71 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, g, redirect, jsonify, send_from_directory
+import json
+import sqlite3
+
+from database import Database
+from exceptions import InvalidUsage
+
+DATABASE = './data/data.db'
 
 app = Flask(__name__)
 
+app.config['UPLOAD_FOLDER'] = "./data"
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return Database(db)
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 @app.route("/")
-def home():
-    return render_template("index.html")
+def home_page():
+    return render_template("index.html", page="home")
+
+@app.route("/new")
+def create_new_project():
+    db = get_db()
+    newID = db.create_new_project(request.args.get("projectName"))
+    if (newID == None):
+        print("Error while creating DB")
+        return redirect("/")
+    else:
+        return redirect("/project/%s" % newID)
+
+
+@app.route("/project/<projectID>")
+def annotater_page(projectID):
+    db = get_db()
+    project = db.get_project_by_ID(projectID)
+    print(project)
+    return render_template("annotate.html", project=project, page="annotate")
+
+@app.route("/project/<projectID>/upload", methods=["POST"])
+def upload_file(projectID):
+    db = get_db()
+    print(request.files)
+    return jsonify(db.add_file_to_project(request.files["file"], projectID))
+
+@app.route("/deleteFile/<fileID>", methods=["POST"])
+def deleteFile(fileID):
+    db = get_db()
+    db.delete_file(fileID)
+    return "Success"
+
+@app.route("/data/<fileID>")
+def getFile(fileID):
+    return send_from_directory("./data", fileID)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
